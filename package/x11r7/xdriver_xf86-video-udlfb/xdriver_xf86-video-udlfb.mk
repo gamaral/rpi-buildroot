1fcfc3782c9e3813d4b764dcac6b4c2a9742225d
Xerxes Rånby <xerxes@gudinna.com>
2014-11-03 11:11:50 +0100

    x11r7/xdriver_xf86-video-udlfb: Make compatible with xorg-server 1.16.99.901
    Signed-off-by: Xerxes Rånby <xerxes@gudinna.com>

--- package/x11r7/xdriver_xf86-video-udlfb/xdriver_xf86-video-udlfb.mk
+++ package/x11r7/xdriver_xf86-video-udlfb/xdriver_xf86-video-udlfb.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-XDRIVER_XF86_VIDEO_UDLFB_SOURCE = master.tar.gz
-XDRIVER_XF86_VIDEO_UDLFB_SITE = https://github.com/xranby/xf86-video-displaylink/archive
+XDRIVER_XF86_VIDEO_UDLFB_VERSION = 5cb3b6c9fb9b3478777631be8c0353b097cbfaf7
+XDRIVER_XF86_VIDEO_UDLFB_SITE = $(call github,xranby,xf86-video-displaylink,$(XDRIVER_XF86_VIDEO_UDLFB_VERSION))
 XDRIVER_XF86_VIDEO_UDLFB_LICENSE = MIT
 XDRIVER_XF86_VIDEO_UDLFB_LICENSE_FILES = COPYING
 XDRIVER_XF86_VIDEO_UDLFB_DEPENDENCIES = xserver_xorg-server xproto_fontsproto xproto_randrproto xproto_renderproto xproto_videoproto xproto_xproto
